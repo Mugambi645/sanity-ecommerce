80b18e6002f61c07019107b9893a69364678b7e3
patrick mugambi <patrick@kali.kali>
2022-11-05 16:50:28 +0300

    set up project structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+const clientSideEmotionCache = createCache({ key: 'css' });
+function MyApp({
+   Component, pageProps, 
+   emotionCache = clientSideEmotionCache
+  }) {
+  return (
+    <CacheProvider value={emotionCache}>
+     <Component {...pageProps} />
+     </CacheProvider>
+     )
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
 }
 
 export default MyApp
