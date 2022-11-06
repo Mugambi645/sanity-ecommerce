2d00469a94ea957b7cd047fcbca0c167fc152f3a
patrick mugambi <patrick@kali.kali>
2022-11-06 10:22:56 +0300

    Create layout component

---
+++ components/Layout.js
@@ -0,0 +1,63 @@
+import { ThemeProvider } from '@emotion/react';
+import { AppBar, CssBaseline, Toolbar, Typography, Link, Container, Box } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import Head from 'next/head';
+import NextLink from 'next/link';
+export default function Layout({title, description, children}) {
+    const theme = createTheme({
+        components: {
+          MuiLink: {
+            defaultProps: {
+              underline: 'hover',
+            },
+          },
+        },
+        typography: {
+          h1: {
+            fontSize: '1.6rem',
+            fontWeight: 400,
+            margin: '1rem 0',
+          },
+          h2: {
+            fontSize: '1.4rem',
+            fontWeight: 400,
+            margin: '1rem 0',
+          },
+        },
+        palette: {
+          mode: 'light',
+          primary: {
+            main: '#f0c000',
+          },
+          secondary: {
+            main: '#208080',
+          },
+        },
+      });
+    return (
+        <>
+             <Head>
+        <title>{title ? `${title} - Sanity ecommerce` : 'Sanity ecommerce'}</title>
+        {description && <meta name="description" content={description}></meta>}
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <AppBar position='static'>
+            <Toolbar>
+            <NextLink href="/" passHref>
+                <Link>
+                <Typography>ecommerce</Typography>
+                </Link>
+                </NextLink>
+            </Toolbar>
+        </AppBar>
+        <Container component='main'>
+            {children}
+        </Container>
+        <Box component="footer">
+          <Typography>All rights reserved. Sanity ecommerce</Typography>
+        </Box>
+      </ThemeProvider>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
 import { Typography } from '@mui/material'
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Sanity ecommerce</title>
-        <meta name="description" content="Ecommerce with sanity and next.js" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-<Typography component="h1" variant="h1">
-  Sanity ecommerce
-</Typography>
-    </div>
+    <Layout>
+      List Products
+    </Layout>
   )
 }
