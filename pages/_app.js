2d370bbb5d8460222256f4a158f8453cb1344381
patrick mugambi <patrick@kali.kali>
2022-11-13 11:33:41 +0300

    implement paypal payment and order history

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
 import StoreProvider from '../utils/Store';
 import { SnackbarProvider } from 'notistack';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 const clientSideEmotionCache = createCache({ key: 'css' });
 function MyApp({
    Component, pageProps, 
@@ -12,7 +13,9 @@ function MyApp({
     <CacheProvider value={emotionCache}>
       <SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
       <StoreProvider>
+      <PayPalScriptProvider deferLoading={true}>
      <Component {...pageProps} />
+     </PayPalScriptProvider>
      </StoreProvider>
      </SnackbarProvider>
      </CacheProvider>
